5c56d70814c34a98306c58ccc8ae506d358628d1
Jesse McDonald <nybble41@gmail.com>
2021-08-03 01:49:24 -0500

    Reimplement Rust test program with a linked list instead of Vec<_>.

--- src/rust/phone_encoder/src/main.rs
+++ src/rust/phone_encoder/src/main.rs
@@ -4,14 +4,13 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-use lazy_static::lazy_static;
-use num_bigint::{BigUint, ToBigUint};
+use num_bigint::BigUint;
 
 type Dictionary = HashMap<BigUint, Vec<String>>;
 
-lazy_static! {
-    static ref ONE: BigUint = 1.to_biguint().unwrap();
-    static ref TEN: BigUint =10.to_biguint().unwrap();
+struct Cons<'a, T: 'a> {
+    data: T,
+    next: Option<&'a Cons<'a, T>>,
 }
 
 /// Port of Peter Norvig's Lisp solution to the Prechelt phone-encoding problem.
@@ -20,75 +19,85 @@ lazy_static! {
 /// due to the very different natures of Lisp and Rust.
 fn main() -> io::Result<()> {
     // drop itself from args
-    let mut args: Vec<_> = args().skip(1).collect();
-    let words_file: String = if !args.is_empty() { args.remove(0) } else { "tests/words.txt".into() };
-    let input_file: String = if !args.is_empty() { args.remove(0) } else { "tests/numbers.txt".into() };
+    let mut args = args().skip(1);
+    let words_file: String = args.next().unwrap_or("tests/words.txt".into());
+    let input_file: String = args.next().unwrap_or("tests/numbers.txt".into());
 
     let dict = load_dict(words_file)?;
 
     for line in read_lines(input_file)? {
         if let Ok(num) = line {
             let digits: Vec<_> = num.chars()
-                .filter(|ch| ch.is_alphanumeric())
+                .filter_map(numeric_char_to_digit)
                 .collect();
-            print_translations(&num, &digits, 0, Vec::new(), &dict)?;
+            print_translations(&dict, &num, &digits, None);
         }
     }
     Ok(())
 }
 
-fn print_translations(
+fn print_translations<'dict>(
+    dict: &'dict Dictionary,
     num: &str,
-    digits: &Vec<char>,
-    start: usize,
-    words: Vec<&String>,
-    dict: &Dictionary,
-) -> io::Result<()> {
-    if start >= digits.len() {
-        print_solution(num, &words);
-        return Ok(());
-    }
-    let mut n = ONE.clone();
-    let mut found_word = false;
-    for i in start..digits.len() {
-        n = &n * (&*TEN) + &nth_digit(digits, i);
-        if let Some(found_words) = dict.get(&n) {
-            for word in found_words {
-                found_word = true;
-                let mut partial_solution = words.clone();
-                partial_solution.push(word);
-                print_translations(num, digits, i + 1, partial_solution, dict)?;
+    digits: &[u8],
+    words: Option<&Cons<&'dict str>>,
+) {
+    if digits.len() == 0 {
+        print!("{}:", num);
+        print_reversed(words);
+        println!();
+    } else {
+        let mut found_word = false;
+        let mut n = 1u8.into();
+        for i in 0..digits.len() {
+            n *= 10u8;
+            n += digits[i];
+            if let Some(ws) = dict.get(&n) {
+                for word in ws {
+                    found_word = true;
+                    print_translations(
+                        dict,
+                        num,
+                        &digits[(i + 1)..],
+                        Some(&Cons {
+                            data: &*word,
+                            next: words,
+                        }),
+                    );
+                }
             }
         }
-    }
-    if !found_word && !words.last().map(|w| is_digit(w)).unwrap_or(false) {
-        let mut partial_solution = words.clone();
-        let digit = nth_digit(digits, start).to_string();
-        partial_solution.push(&digit);
-        print_translations(num, digits, start + 1, partial_solution, dict)
-    } else {
-        Ok(())
-    }
-}
-
-fn print_solution(num: &str, words: &Vec<&String>) {
-    // do a little gymnastics here to avoid allocating a big string just for printing it
-    print!("{}", num);
-    if words.is_empty() {
-        println!(":");
-        return;
-    }
-    print!(": ");
-    let (head, tail) = words.split_at(words.len() - 1);
-    for word in head {
-        print!("{} ", word);
-    }
-    for word in tail { // only last word in tail
-        println!("{}", word);
+        if !found_word
+            && !words
+                .map(|c| c.data.chars().all(char::is_numeric))
+                .unwrap_or(false)
+        {
+            print_translations(
+                dict,
+                num,
+                &digits[1..],
+                Some(&Cons {
+                    data: digit_to_str(digits[0]),
+                    next: words,
+                }),
+            );
+        }
     }
 }
 
-fn load_dict(words_file: String) -> io::Result<Dictionary> {
+fn print_reversed(words: Option<&Cons<&str>>) {
+    if let Some(c) = words {
+        print_reversed(c.next);
+        print!(" {}", c.data);
+    }
+}
+
+fn digit_to_str(digit: u8) -> &'static str {
+    &"0123456789"[digit as usize..][..1]
+}
+
+fn load_dict<P>(words_file: P) -> io::Result<Dictionary>
+where P: AsRef<Path> {
     let mut dict = HashMap::with_capacity(100);
     let words = read_lines(words_file)?;
     for line in words {
@@ -104,32 +113,22 @@ fn load_dict(words_file: String) -> io::Result<Dictionary> {
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-    where P: AsRef<Path>, {
+where P: AsRef<Path> {
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 fn word_to_number(word: &str) -> BigUint {
-    let mut n = ONE.clone();
-    for ch in word.chars() {
-        if ch.is_alphabetic() {
-            n = &n * (&*TEN) + &char_to_digit(ch);
-        }
+    let mut n = 1u8.into();
+    for digit in word.chars().filter_map(alpha_char_to_digit) {
+        n *= 10u8;
+        n += digit;
     }
     n
 }
 
-fn nth_digit(digits: &Vec<char>, i: usize) -> BigUint {
-    let ch = digits.get(i).expect("index out of bounds");
-    ((*ch as usize) - ('0' as usize)).to_biguint().unwrap()
-}
-
-fn is_digit(string: &str) -> bool {
-    string.len() == 1 && string.chars().next().unwrap().is_digit(10)
-}
-
-fn char_to_digit(ch: char) -> u32 {
-    match ch.to_ascii_lowercase() {
+fn alpha_char_to_digit(ch: char) -> Option<u8> {
+    Some(match ch.to_ascii_lowercase() {
         'e' => 0,
         'j' | 'n' | 'q' => 1,
         'r' | 'w' | 'x' => 2,
@@ -140,6 +139,15 @@ fn char_to_digit(ch: char) -> u32 {
         'b' | 'k' | 'u' => 7,
         'l' | 'o' | 'p' => 8,
         'g' | 'h' | 'z' => 9,
-        _ => panic!("invalid input: not a digit: {}", ch)
+        _ => return None,
+    })
+}
+
+fn numeric_char_to_digit(ch: char) -> Option<u8> {
+    if ch >= '0' && ch <= '9' {
+        Some(((ch as usize) - ('0' as usize)) as u8)
+    }
+    else {
+        None
     }
 }
